296f06ff3f528a7fa76b1973d3c147aaff15b33d
Samuel LaBrant <sjlabrant@gmail.com>
2024-10-12 14:12:27 -0500

    More of a game.

--- scenes/cart.gd
+++ scenes/cart.gd
@@ -1,22 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 50.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("input_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("input_left", "input_right")
+	var direction := 1
 	if direction:
 		velocity.x = direction * SPEED
 	else:
