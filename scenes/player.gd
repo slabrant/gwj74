1b7266e383c4aa5209ff1431a75c10c7c35f2560
Samuel LaBrant <sjlabrant@gmail.com>
2024-10-12 15:54:41 -0500

    New jump sound.

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
@@ -13,6 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("input_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
